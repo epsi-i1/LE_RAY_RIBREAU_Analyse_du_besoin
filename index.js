ee636435bcf723a356c2b23279baab059667a13a
Augustin Ribreau <augustin.ribreau@gmail.com>
2021-10-12 10:42:58 +0200

    feat: reformat participant

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const moment = require('moment');
 const Session = require('./classes/Session');
+
+/**
+ * @param {Array} data
+ * @returns [{id: String, lastname: String, firstname: String, wight: Number, year: Number},*]
+ */
+function reformatParticipant(data) {
+    return data.map((participant, index) => {
+        if (participant.includes(null)) {
+            return
+        }
+        return new Participant(participant);
+    });
+}
+
 /**
  * @param {[]} participants
  * @returns {Session}
  */
 function createSession(participants) {
-    return new Session(participants);
+    const participantsFormatted = reformatParticipant(participants);
+
+    return new Session(participantsFormatted);
+}
 }
 
